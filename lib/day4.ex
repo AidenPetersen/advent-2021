ad779ee8339a9282e03a8e51144fedfbba6f3cbc
Aiden Petersen <wdaidenp@iastate.edu>
2021-12-10 14:31:02 -0600

    Working on day4p1

--- lib/day4.ex
+++ lib/day4.ex
@@ -1,5 +1,77 @@
 defmodule Day4 do
   def part1 do
-    defp check_bingo(board)
+  end
+
+  defp mark_board(board, num) do
+    board
+    |> Enum.map(fn x ->
+      Enum.map(x, fn {y, b} ->
+        if y == num do
+          {y, true}
+        else
+          {y, b}
+        end
+      end)
+    end)
+  end
+
+  defp check_bingo(board) do
+    horiz =
+      board
+      |> Enum.map(fn x ->
+        Enum.all?(x, fn {_, b} -> b end)
+      end)
+      |> Enum.any?()
+
+    vert =
+      board
+      |> List.zip()
+      |> Enum.map(&Tuple.to_list/1)
+      |> Enum.map(fn x ->
+        Enum.all?(x, fn {_, b} -> b end)
+      end)
+      |> Enum.any?()
+
+    diagonal1 = Enum.zip(Enum.to_list(0..4), Enum.to_list(0..4))
+    diagonal2 = Enum.zip(Enum.to_list(4..0), Enum.to_list(0..4))
+
+    d1 =
+      Enum.map(diagonal1, fn {x, y} ->
+        {_, b} = board |> Enum.at(y) |> Enum.at(x)
+        b
+      end)
+
+    d2 =
+      Enum.map(diagonal2, fn {x, y} ->
+        {_, b} = board |> Enum.at(y) |> Enum.at(x)
+        b
+      end)
+
+    horiz || vert || d1 || d2
+  end
+
+  def get_input(file) do
+    [nums | boards] =
+      File.read!(file)
+      |> String.split("\n\n", trim: true)
+
+    nums =
+      nums
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer(&1))
+
+    boards =
+      boards
+      |> Enum.map(fn x -> String.split(x, "\n", trim: true) end)
+      |> Enum.map(fn x ->
+        Enum.map(x, fn y ->
+          y
+          |> String.split(~r{\s}, trim: true)
+          |> IO.inspect()
+          |> Enum.map(&{String.to_integer(&1), false})
+        end)
+      end)
+
+    {nums, boards}
   end
 end
